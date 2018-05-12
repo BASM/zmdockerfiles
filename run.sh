a8ad4b3832ad4d16c7fa92b8e4f53abb3da53962
Leonid Muravjev <asm@asm.pp.ru>
2018-05-12 18:30:06 +0300

    Added Europe/Moscow zone

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ! docker run -d -t -p 1080:80 --publish-all=true --name zoneminger zoneminder/zoneminder
+if ! docker run -d -t -p 1080:80 --publish-all=true --name zoneminger -e TZ="Europe/Moscow" zoneminder/zoneminder
 then
 	docker start zoneminger
 fi
